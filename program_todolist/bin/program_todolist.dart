0e6f0e95d058f366e3d9104e1d725de2c9aa1d5b
meryymarta <meriammartadinata@gmail.com>
2026-03-10 08:18:10 +0700

    Commit 1: Inisialisasi struktur Program To-Do List

---
+++ program_todolist/bin/program_todolist.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+void main(){
+  print("--- Program To-Do List ---"); 
+  List<String> daftarTugas = [];
+}
---
+++ program_todolist/test/program_todolist_test.dart
@@ -0,0 +1,8 @@
+import 'package:program_todolist/program_todolist.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
