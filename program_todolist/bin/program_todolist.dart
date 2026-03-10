11c175205dd13dfa2a4428dc24bd1a15da740f81
meryymarta <meriammartadinata@gmail.com>
2026-03-10 08:43:22 +0700

    Commit 3: Menambahkan percabangan switch-case untuk navigasi menu

--- program_todolist/bin/program_todolist.dart
+++ program_todolist/bin/program_todolist.dart
@@ -13,6 +13,23 @@ void main(){
 
     String? pilihan = stdin.readLineSync();
 
+    switch (pilihan) {
+      case '1':
+      print("Fitur tambah tugas sedang dibuat...");
+      break;
+      case 2:
+      print("Fitur lihat daftar tugas sedang dibuat...");
+      break;
+      case 3:
+      print("Terimaksih sudah menggunakan program To-Do List! Jangan lupa diselesaikan tugasnya ^^");
+      isRunning = false;
+      break;
+      default:
+      print("Pilihan kamu engga valid nih. Silakan coba lagi.");
+
+
+    }
+
   }
 
   
