8d048a7a059439ea0c32296c9933e504991167a8
meryymarta <meriammartadinata@gmail.com>
2026-03-10 08:32:46 +0700

    Commit 2: menambahkan perulangan while untuk menu utama

--- program_todolist/bin/program_todolist.dart
+++ program_todolist/bin/program_todolist.dart
@@ -3,4 +3,17 @@ import 'dart:io';
 void main(){
   print("--- Program To-Do List ---"); 
   List<String> daftarTugas = [];
+
+  bool (isRunning) {
+    print("\nMenu:");
+    print("1. Tambah Tugas Baru");
+    print("2. Lihat Daftar Tugas");
+    print("3. Keluar");
+    stdout.write("Ketik pilihanmu (1/2/3):");
+
+    String? pilihan = stdin.readLineSync();
+
+  }
+
+  
 }
